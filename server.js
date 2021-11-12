fc6aaabe97bb0c03ea1adfbcf0099e0e26781458
Daniel Rodriguez <danielrodriguez@Daniels-MBP.home>
2021-11-12 17:39:38 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+//dependencies 
+require("dotenv").config()
+
+
+// port variable
+const {PORT = 3000, MONGODB_URL} = process.env
+
+const express = require("express")
+
+const app = express()
+const mongoose = require("mongoose")
+
+
+
+//import middleware
+
+const cors = require("cors") //cors headers, gives frontend permission to use this backend
+const morgan = require("morgan") //logging , better for diagnosing errors and fixing them 
+
+
+
+
+//database connection
+mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+
+mongoose.connection
+.on("open", () => console.log("Connected to Mongo"))
+.on("close", () => console.log("Disconnected to Mongo"))
+.on("error", (error) => console.log(error))
+
+
+//register middleware
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+
+//routes
+app.get("/", (req, res) =>{
+    res.send("Hello world")
+})
+
+
+//server listener
+app.listen(PORT, () => {console.log(`Listening on ${PORT}`)})
